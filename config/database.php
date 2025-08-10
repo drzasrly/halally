a874a4243620c6f0974d8b011fb1980ddc4679c3
elga <elgarolobessy@gmail.com>
2025-08-10 17:45:53 +0700

    gambar-barang

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-    $host = "localhost:3307";
+    $host = "localhost:3306";
     $user = "root";
     $password = "";
 
-    $db = "halal";
+    $db = "halally";
     $kon = mysqli_connect($host, $user, $password, $db);
     if (!$kon){
           die("Koneksi gagal:".mysqli_connect_error());
